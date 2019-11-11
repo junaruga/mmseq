b92b18b836e00269c3a4c8039171e43dd93711bc
Jun Aruga <jaruga@redhat.com>
2019-11-11 22:04:29 +0100

    Remove atlas and samtools dependencies.

--- test.sh
+++ test.sh
@@ -3,14 +3,6 @@
 set -ex
 
 pushd src
-# In case of Fedora environment
-if [ -f /usr/lib64/atlas/libatlas.a ]; then
-    sed -i 's|-latlas|/usr/lib64/atlas/libatlas.a|' Makefile
-fi
-# Test with the samtools latest stable version.
-git clone -b 1.9 https://github.com/samtools/samtools.git
-sed -i 's|#include "sam.h"|#include "samtools/sam.h"|' bam2hits.cpp
-
 make clean
 make
 popd
